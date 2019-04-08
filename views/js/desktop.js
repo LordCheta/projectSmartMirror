301ea7e620d77cd5210583f8b76b1b80065ed1f8
je-ni <jenniferolibie@gmail.com>
2019-04-08 17:18:38 +0100

    changed time to 12hr format

--- views/js/desktop.js
+++ views/js/desktop.js
@@ -10,16 +10,23 @@ window.onload = () => {
     
     // Desktop Funtionalities
     let showDateTime = async () => {
+        let amPm = 'am';
         let desktopDate = new Date();
-        let hour = desktopDate.getHours();
-        let min = desktopDate.getMinutes();
+        // for the hour
+        let hour = 24;
+        if ( hour > 12 ) {
+            hour -= 12;
+            amPm = 'pm';
+        }
+        let min = desktopDate.getMinutes() < 10 ? `0${desktopDate.getMinutes()}` : desktopDate.getMinutes();
         let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
         let day = days[desktopDate.getDay()];
         let date =desktopDate.getDate()
         let months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
         let month = months[desktopDate.getMonth()];
         console.log(desktopDate);
-        dom.timeDisplay.innerHTML = `${hour}:${min}`;
+    
+        dom.timeDisplay.innerHTML = `${hour}:${min}${amPm}`;
         dom.dateDisplay.innerHTML = `${day}, ${date} ${month}`
     }
     
