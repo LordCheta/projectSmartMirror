30db56276116051d8903725302b48841f5b24920
je-ni <jenniferolibie@gmail.com>
2019-08-30 20:52:42 +0100

    added alert for starting and stopping speech

--- voice/entry.js
+++ voice/entry.js
@@ -2,21 +2,41 @@
 // since it's electron means chrome only
 
 // have a voice button that one clicks when they want to turn on speech recogntion on home
-// const mic = document.querySelector('#microphone');
+const mic = document.querySelector('#mic');
+const alert = document.querySelector('#micAlert');
 let recognition = new webkitSpeechRecognition();
+
 // end recognition when user stops speaking
 recognition.continuous = false;
+
 // show intermediate processing
 recognition.interimResults = false;
 
-recognition.onresult = function(event){
-  console.log(event)
-}
-
-recognition.start();
-
-mic.addEventListener("click", ()=> start()); //add click event to mic
-// when a word is recognized, perform action and then start recognition again
-recognition.onend = () => {
+const start = () => {
   recognition.start();
+
+  recognition.onresult = function(event){
+    console.log(event)
+  }
 }
+
+// when a word is recognized, perform action and then start recognition again
+// recognition.onend = () => {
+//   recognition.start();
+// }
+mic.addEventListener("click", ()=> {
+  if (mic.className == '') {
+    mic.className = "started";  
+    start();
+    micAlert.innerText = 'Speech Recognition started. Click Mic again to stop';
+    micAlert.style.display = 'block';
+    setTimeout(()=> micAlert.style.display = 'none', 5000)
+  } else {
+    recognition.abort();
+    mic.className = '';
+    micAlert.innerText = 'Speech Recognition stopped. Click Mic again to start';
+    micAlert.style.display = 'block';
+    setTimeout(()=> micAlert.style.display = 'none', 5000)
+  }
+}); //add click event to mic
+// add a property to mic to indicate when recognition is on or off
