4702344242bde9bed57b5aa055b702b61aac800c
je-ni <jenniferolibie@gmail.com>
2019-09-13 10:34:59 +0100

    the VR is not workingggggggg ;(

--- voice/entry.js
+++ voice/entry.js
@@ -2,21 +2,26 @@
 // since it's electron means chrome only
 
 // have a voice button that one clicks when they want to turn on speech recogntion on home
+
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 const mic = document.querySelector('#mic');
 const alert = document.querySelector('#micAlert');
-let recognition = new webkitSpeechRecognition();
+let recognition = new SpeechRecognition();
 
 // end recognition when user stops speaking
 recognition.continuous = false;
 
 // show intermediate processing
-recognition.interimResults = false;
+recognition.interimResults = true;
 
 const start = () => {
   recognition.start();
-
-  recognition.onresult = function(event){
-    console.log(event)
+  
+  console.log('gg')
+  recognition.onresult = async function(event){
+    console.log('here');
+    const STT = await event.results[0][0];
+    console.log(STT);
   }
 }
 
@@ -24,13 +29,18 @@ const start = () => {
 // recognition.onend = () => {
 //   recognition.start();
 // }
+
+//add click event to mic
 mic.addEventListener("click", ()=> {
   if (mic.className == '') {
-    mic.className = "started";  
     start();
+
+    // add a property to mic to indicate when recognition is on or off
+    mic.className = "started";  
     micAlert.innerText = 'Speech Recognition started. Click Mic again to stop';
     micAlert.style.display = 'block';
     setTimeout(()=> micAlert.style.display = 'none', 5000)
+  
   } else {
     recognition.abort();
     mic.className = '';
@@ -38,5 +48,4 @@ mic.addEventListener("click", ()=> {
     micAlert.style.display = 'block';
     setTimeout(()=> micAlert.style.display = 'none', 5000)
   }
-}); //add click event to mic
-// add a property to mic to indicate when recognition is on or off
+}); 
